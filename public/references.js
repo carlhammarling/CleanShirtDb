e0c912d9b49955754a417206ba7c4b8e2fe0a72f
Carl Hammarling <carlhammarling@gmail.com>
2023-04-12 13:38:23 +0200

    Fixed reviews

--- public/references.js
+++ public/references.js
@@ -2,8 +2,6 @@ const id = new URLSearchParams(window.location.search).get('id')
 const output = document.querySelector('#output')
 
 
-
-
 const getOneProduct = async () => {
     const res = await fetch('./api/products/' + id)
     const data = await res.json()
@@ -14,12 +12,14 @@ const getOneProduct = async () => {
 getOneProduct()
 
 const listProduct = (data) => {
-    // output.innerHTML = ''
+    output.innerHTML = ''
 
     const img = document.createElement('img')
     img.setAttribute('src', data.imgURL)
 
     const section = document.createElement('section')
+    const productInfo = document.createElement('div')
+    productInfo.className = 'productInfo'
     
     const title = document.createElement('h1')
     title.innerText = 'Reviews'
@@ -29,18 +29,50 @@ const listProduct = (data) => {
 
    
     //Likes
-    const rating = data.rating;
-    const solidStars = '<i class="fa-solid fa-star"></i>'.repeat(rating)
-    const regularStars = '<i class="fa-regular fa-star"></i>'.repeat(5-rating)
+    // const rating = data.rating;
+    const ratingArray = [];
+    data.comments.forEach(comment => {
+        ratingArray.push(comment.rating)
+    })
+    const ratingSum = Math.ceil(ratingArray.reduce((total, num) => total + num, 0) / ratingArray.length)
+    console.log(ratingSum)
+
+    const solidStars = '<i class="fa-solid fa-star"></i>'.repeat(ratingSum)
+    const regularStars = '<i class="fa-regular fa-star"></i>'.repeat(5-ratingSum)
     const stars = document.createElement('h3')
     stars.innerHTML = solidStars + regularStars
 
     const description = document.createElement('p')
     description.innerText = data.description
 
+    productInfo.append(title, name, stars, description)
+
+    //Build reviews
+
+    const reviewList = document.createElement('div')
+    reviewList.className = 'reviewList'
+
+    data.comments.forEach(comment => {
+
+        const review = document.createElement('div')
+        review.className = 'review'
+
+        const rating = comment.rating;
+        const solidStars = '<i class="fa-solid fa-star"></i>'.repeat(rating)
+        const regularStars = '<i class="fa-regular fa-star"></i>'.repeat(5-rating)
+        const userName = document.createElement('h4')
+        userName.innerHTML = `${comment.userId.firstName} ${comment.userId.lastName} ${solidStars}${regularStars}`
+
+        const reviewText = document.createElement('p')
+        reviewText.innerText = comment.comment
 
 
-    section.append(title, name, stars, description)
+        review.append(userName, reviewText)
+        reviewList.append(review)
+    })
+
+
+    section.append(productInfo, reviewList)
     output.append(img, section)
 }
 
@@ -59,11 +91,7 @@ const listProduct = (data) => {
         // <article>
         //         <img src="./styles/images/pexels-spencer-selover-428311.jpg" alt="Shirt">      
         //         <section>
-        //             <h1>Reviews</h1>
-        //             <h2>CLEAN SHiRT - Blue with text.</h2>
-        //             <h3><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i></h3>
-        //             <br>
-        //             <h4>Sean Connory <i class="fa-solid fa-star fa-sm"></i><i class="fa-regular fa-star fa-sm"></i><i class="fa-regular fa-star fa-sm"></i><i class="fa-regular fa-star fa-sm"></i><i class="fa-regular fa-star fa-sm"></i></h4>
+        //             
         //             <p>Apparently this one doesn't highlight my features very well, I will keep to suit and tie. Will not buy again.
         //             </p>
         //             <br>
